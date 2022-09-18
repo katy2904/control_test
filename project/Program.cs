d2fda6aa0d78796a64b43a7ee883669b4aee7415
Katy <wc-katy@yandex.ru>
2022-09-18 21:08:05 +0700

    add programm

---
+++ project/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[size];
+
+int count = 0;
+string result = string.Empty;
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = Console.ReadLine();
+    if (array[i].Length <= 3)
+    {
+        count++;
+        result += array[i] + " ";
+    }
+}
+
+string[] finalArray = result.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine($"В массиве[{String.Join(", ", array)}] элемента длиной "
+                    +$"до 3-х символов включительно - {count}");
+Console.WriteLine($"[{String.Join(", ", finalArray)}]");
